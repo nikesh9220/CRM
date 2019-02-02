c958fdf8dcea7ca5c5f779fcbfe1e626a37bb5f1
Mihir Naik <mihir.naik@exxatsystems.com>
2019-02-02 21:01:47 +0530

    first commit

---
+++ OnBoardCRM.BL/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoardCRM.BL
+{
+    public class Class1
+    {
+        public int X { get; set; }
+
+        public void Fetch()
+        {
+            OnBoardCRM.DL.Class2 obj = new DL.Class2();
+        }
+    }
+}
---
+++ OnBoardCRM/Class1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnBoardCRM
+{
+    public partial class Class1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OnBoardCRM.BL.Class1 obj = new BL.Class1();
+        }
+    }
+}
