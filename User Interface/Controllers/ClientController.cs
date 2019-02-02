a03627e39f1e99c1f0388f7bf3d67c3ccf347a65
Nilkanth Bajaniya <nilbajaniya@gmail.com>
2019-02-03 03:39:02 +0530

    This is msg

---
+++ User Interface/Controllers/ClientController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace User_Interface.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewClient()
+        {
+
+            return View("NewClientForm");
+        }
+    }
+}
